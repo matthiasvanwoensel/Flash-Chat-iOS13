195d9f120d61c3b80f774396aa3a7660094660af
matthiasvanwoensel <matthias@fluvio.be>
2022-06-29 14:48:35 +0200

    message cell layout

---
+++ Flash Chat iOS13/Views/MessageCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCellTableViewCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Matthias Van Woensel on 29/06/2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
