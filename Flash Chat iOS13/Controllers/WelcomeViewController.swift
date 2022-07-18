23dfd57ee06e8ad9433c4a58441b2e5511c1cfd3
matthiasvanwoensel <matthias@fluvio.be>
2022-07-18 16:38:04 +0200

    afwerking

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -13,6 +13,18 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: CLTypingLabel!
     
+
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // dissable navigation bar
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = false
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
